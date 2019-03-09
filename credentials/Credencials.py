27da4d976a652137e9be256cf38b5e939ce34843
Arthur.mf <arthurmf.amf@gmail.com>
2019-03-09 18:26:43 +0100

    chnage repo

--- credentials/Credencials.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  7 20:02:22 2019
-
-@author: arthurmaroquenefroissart
-"""
-
--- credentials/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  7 20:01:44 2019
-
-@author: arthurmaroquenefroissart
-"""
-
