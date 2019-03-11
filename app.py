09e01397132f43bcda38e043a2746fdb37fd0695
Arthur.mf <arthurmf.amf@gmail.com>
2019-03-11 12:19:48 +0100

    Test

--- app.py
+++ app.py
@@ -15,78 +15,16 @@ import plotly
 import flask
 import pandas as pd
 import numpy as np
-app = dash.Dash()
+
 #%% COnnect to Post GRE Database 
 
 conn = pg.connect(database="postgres", user = "postgres",password = "", host = "127.0.0.1", port = "5432")
 print ("Opened database successfully")
 df = pd.read_sql_query('select * from "Test1_1";', conn)
-map_data=df
 
 #%% Set Up Layout 
-
-# Boostrap CSS.
-app.css.append_css({'external_url': 'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'})
-
-layout = dict(
-    autosize=True,
-    height=500,
-    font=dict(color="#191A1A"),
-    titlefont=dict(color="#191A1A", size='14'),
-    margin=dict(
-        l=35,
-        r=35,
-        b=35,
-        t=45
-    ),
-    hovermode="closest",
-    plot_bgcolor='#fffcfc',
-    paper_bgcolor='#fffcfc',
-    legend=dict(font=dict(size=10), orientation='h'),
-    title='Each dot is an NYC Middle School eligible for SONYC funding',
-    mapbox=dict(
-        #accesstoken=mapbox_access_token,
-        style="light",
-        center=dict(
-            lon=-73.91251,
-            lat=40.7342
-        ),
-        zoom=10,
-    )
-)
-        
+   
 #%% BUilding the App 
-    
-
-# Map + table + Histogram
-html.Div(
-    [
-        html.Div(
-            [
-                dcc.Graph(id='map-graph',
-                          style={'margin-top': '20'})
-            ], className = "six columns"
-        ),
-        html.Div(
-            [
-                dt.DataTable(
-                    rows=map_data.to_dict('records'),
-                    columns=map_data.columns,
-                    row_selectable=True,
-                    filterable=True,
-                    sortable=True,
-                    selected_row_indices=[],
-                    id='datatable'),
-            ],
-            #style=layout_right,
-            className="six columns"
-        ),
-        html.Div(
-            [
-                dcc.Graph(id="histogram")
-            ],className="twelve columns")
-    ], className="row"
-)
 
 #%%
 app = dash.Dash(__name__)
